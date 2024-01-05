399595d27312ea12b2a6feaa17b670e2608820dd
zkssh <ponthiack@protonmail.com>
2024-01-04 23:09:58 -0600

    saving changes

--- code/200.number-of-islands.java
+++ code/200.number-of-islands.java
@@ -1,8 +1,8 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int count = 0;
-        for (int i=0; i<grid.lenghtl; i++) {
-            for (int j=0; j<grid[i].lenght; j++) {
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[i].length; j++) {
                 if (grid[i][j] == '1') {
                     count += 1;
                     callBS(grid, i, j);
@@ -10,18 +10,19 @@ class Solution {
             }
         }
         return count;
-
-        public void callBS(char[][] grid; int i, int j) {
-            if (i < 0 || i>=grid.lenght || j<0 || j>=grid[i].lenght || grid[i][j] == 0)
-                return;
-
-            grid[i][j] = '0';
-            callBS(grid, i+1, j) // up
-            callBS(grid, i-1, j) //down
-            callBS(grid, i, j-1) //left
-            callBS(grid, i, j+1) //right
-        }
-
-
+        
     }
+    public void callBS(char[][] grid, int i, int j) {
+        if (i < 0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j] == 0)
+            return;
+
+        grid[i][j] = '0';
+        callBS(grid, i+1, j); // up
+        callBS(grid, i-1, j); //down
+        callBS(grid, i, j-1); //left
+        callBS(grid, i, j+1); //right
+    }
+
+
+    
 }
